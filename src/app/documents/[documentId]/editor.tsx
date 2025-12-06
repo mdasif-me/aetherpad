669325208e60abf57ac1704fb502c137756213de
mdasif-me <cn.mdasif@gmail.com>
2025-12-06 14:02:00 +0600

    feat: add Ruler component for visual measurement in editor

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -19,9 +19,11 @@ import ImageResize from 'tiptap-extension-resize-image';
 import { FontSizeExtension } from '../../../extensions/font-size';
 import { LineHeightExtension } from '../../../extensions/line-height';
 import { useEditorStore } from '../../../store/use-editor-store';
+import Ruler from './ruler';
 const Editor = () => {
   const { setEditor } = useEditorStore();
   const editor = useEditor({
+    immediatelyRender: false,
     onCreate: ({ editor }) => {
       setEditor(editor);
     },
@@ -172,6 +174,7 @@ const Editor = () => {
   });
   return (
     <div className='size-full overflow-x-auto bg-[#f9fbfd] px-4 print:p-0 print:bg-white print:overflow-visible'>
+      <Ruler />
       <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
         <EditorContent editor={editor} />
       </div>
